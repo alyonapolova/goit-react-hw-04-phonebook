ab449a44643e5d0849b87b88cab679d9662223bf
Alyona <polova_alyona0907@ukr.net>
2023-09-23 21:05:56 +0300

    done App hooks
    problem with newContact. It doesnt add it to LocalStor, and filter doesnt work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,81 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ContactForm } from './Form/Form';
 import { Filter } from './Form/Filter';
 import { ContactList } from './Form/ContactList';
 import React from 'react';
 
 import { FormDiv, ContactsDiv } from './Form/Form.styled';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  // state = {
+  //   contacts: [],
+  //   filter: '',
+  // };
 
-  componentDidMount() {
+  useEffect(() => {
     const allContacts = localStorage.getItem('contacts');
-    if (allContacts && JSON.parse(allContacts)) {
-      this.setState({ contacts: JSON.parse(allContacts) });
-    } else this.setState(this.state.contacts);
-  }
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+    if (allContacts) setContacts(JSON.parse(allContacts));
+  }, []);
 
-  handleAddContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+  // componentDidMount() {
+  //   const allContacts = localStorage.getItem('contacts');
+  //   if (allContacts && JSON.parse(allContacts)) {
+  //     this.setState({ contacts: JSON.parse(allContacts) });
+  //   } else this.setState(this.state.contacts);
+  // }
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // componentDidUpdate(_, prevState) {
+  //     if (prevState.contacts.length !== this.state.contacts.length) {
+  //       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //     }
+  //   }
+
+  const handleAddContact = newContact => {
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
-  handleFilterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
+  // handleAddContact = newContact => {
+  //   this.setState(prevState => ({
+  //     contacts: [...prevState.contacts, newContact],
+  //   }));
+  // };
+
+  const handleFilterChange = e => {
+    setFilter(e.currentTarget.value);
   };
-  handleDelete = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
+  // handleFilterChange = e => {
+  //   this.setState({ filter: e.currentTarget.value });
+  // };
+
+  const handleDelete = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  // handleDelete = id => {
+  //   this.setState(prev => ({
+  //     contacts: prev.contacts.filter(contact => contact.id !== id),
+  //   }));
+  // };
 
-    return (
-      <>
-        <FormDiv>
-          <h1>Phonebook</h1>
-          <ContactForm
-            onSubmit={this.handleAddContact}
-            contacts={this.state.contacts}
+  return (
+    <>
+      <FormDiv>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={handleAddContact} contacts={contacts} />
+        <h2>Contacts</h2>
+        <ContactsDiv>
+          <Filter value={filter} onChange={handleFilterChange} />
+          <ContactList
+            contacts={contacts}
+            filter={filter}
+            handleDelete={handleDelete}
           />
-          <h2>Contacts</h2>
-          <ContactsDiv>
-            <Filter value={filter} onChange={this.handleFilterChange} />
-            <ContactList
-              contacts={contacts}
-              filter={filter}
-              handleDelete={this.handleDelete}
-            />
-          </ContactsDiv>
-        </FormDiv>
-      </>
-    );
-  }
-}
+        </ContactsDiv>
+      </FormDiv>
+    </>
+  );
+};
